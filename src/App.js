ba5b1cfe6667d920b24fd50a904d69946e683b28
gestebarena <gestebarena@gmail.com>
2022-11-29 09:46:18 +0200

    icons

--- src/App.js
+++ src/App.js
@@ -27,8 +27,7 @@ function App() {
     
     <div className="App">
       <header className="App-header">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <ActivePage/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>        <ActivePage/>
       </header>
     </div>
   );
